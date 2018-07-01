c9e3a15cab0fe3c0d0cb31cdf4756a264bde7248
Salmaan Rashid <salrashid123@gmail.com>
2018-07-01 16:44:33 -0700

    add iamcredential api

--- id_token/iam_svc_tokens/main.py
+++ id_token/iam_svc_tokens/main.py
@@ -1,71 +1,3 @@
-## ID Token
-
-
-The following section details how to create an verify various id_tokens.  These tokens comes in various flavors:
-
-* google service account id_tokens
-* [OpenIDConnect ID token](https://developers.google.com/actions/reference/rest/Shared.Types/AssertionType)
-
-
-The samples here outline how to get an id_token using your existing credentials and ones based off a service_account_json file.
-
-The service_account based id_tokens maybe exchanged with google for an OpenIDConnect token.
-
-To use, download a service_account json certificate file, then edit the sample in question and replace the certificate path within the code
-(sorry, i didn't make it a arg)
-
-
-The output of each script displays an id_token for the current user's gcloud login, then an id_token for the service account, and finally the same id_token
-for a service account exchanged for a google-id-token.  In each case, the token is verified against the public certificates:
-
-eg verified against public certs in x509 format:
-
-```
-const service_account_certs = 'https://www.googleapis.com/service_accounts/v1/jwk/svc-2-429@mineral-minutia-820.iam.gserviceaccount.com';
-const google_certs = 'https://www.googleapis.com/oauth2/v3/certs';
-```
-
-- golang:  see [main.go](golang/main.go)
-  ```
-  go get github.com/coreos/go-oidc golang.org/x/net/context golang.org/x/oauth2 golang.org/x/oauth2/google golang.org/x/oauth2/jws google.golang.org/api/oauth2/v2 google.golang.org/grpc/credentials/oauth
-
-  go run src/main.go
-  ```
-
-- python:  see [main.py](python/main.py)
-  ```
-  virtualenv env
-  source env/bin/activate
-  pip install -r requirements.txt
-
-  python main.py
-  ```
-
-- java:  see [TestApp.java](java/src/main/java/com/test/TestApp.java)
-  ```
-  mvn clean install exec:java -q
-  ```
-
-- node:  see [app.js](node/app.js)
-  ```
-  npm i
-
-  node user.js   (gets a users gcloud id_token and verifies it)
-  node service.js   (gets a service accounts id_token and verifies it with the service accounts public key)
-  node google-id-token.js   (gets a service accounts id_token and exchanges it for a google-id-token...then verifies it)    
-  ```  
-
-- dotnet:  see [Program.cs](dotnet/Program.cs)
-  Unimplimented
-
-Please also see [Using Service Account Actor for Account Impersonation](../auth/tokens)
-
-
->>Update 7/1/18: GCP IAM now allows for [IAMCredentials.generateAccessToken()](https://cloud.google.com/iam/docs/creating-short-lived-service-account-credentials). This feature allows you to directly genreate an ID or AccessToken and place conditionals on delegation too. While the samples below will work, I’ll rework the sample in this repo to use that API as shown in this example. In several ways, this article is obsolete given the direct API is now available. I’ll keep it posted as a reference. The full code sample for iamcredentials API is shows at the end of the article
-
-see
-
-```python
 import logging
 import os
 import sys
@@ -87,7 +19,7 @@ svc_account_C = 'id-service-account-c@mineral-minutia-820.iam.gserviceaccount.co
 
 
 # initialize root creds for A
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/path/to/svc_account.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/srashid/gcp_misc/certs/mineral-minutia-820-83b3ce7dcddb.json"
 
 project_id ='-'
 
@@ -135,6 +67,8 @@ print 'iamcredentials.generateAccessToken():  --------------------- '
 
 print resp
 
+
+
 body = {
   "delegates": [],
   "audience": svc_account_B,
@@ -149,7 +83,12 @@ print 'iamcredentials.generateIdToken():  --------------------- '
 print resp
 
 
+
+
 print '=========================== delegation =================================='
+
+
+
 
 body={
   "delegates": [
@@ -184,5 +123,3 @@ resp = req.execute()
 print 'iamcredentials.generateIdToken():  --------------------- '
 
 print resp
-
-```
